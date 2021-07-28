c36e7574eb7c351e15285c560ab6437c0bb74e70
Beňuš <ondrej.benus@rechip.eu>
2021-07-28 17:21:54 +0200

    feat: basic template

---
+++ zeeno/hello-world/include/main.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef ZEENO_HELLO_WORLD_MAIN_HPP
+#	define ZEENO_HELLO_WORLD_MAIN_HPP
+
+/**
+ * @file Dummy file
+ * This file is here just as a placeholder
+*/
+
+#endif
---
+++ zeeno/hello-world/src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	std::cout << "Hell"
+	          << "o world" << std::endl;
+	return EXIT_SUCCESS;
+}
